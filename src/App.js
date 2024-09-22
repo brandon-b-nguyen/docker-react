b5dece6596240873b56bcc49e702c56a940133cd
brandon-b-nguyen <the.brandon.nguyen@gmail.com>
2024-09-22 04:48:36 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ewewhgewhw
+          Edit <code>src/App.js</code> and save to reload please woooork.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          hmmmm
+          Learn React pleassse work
         </a>
       </header>
     </div>
